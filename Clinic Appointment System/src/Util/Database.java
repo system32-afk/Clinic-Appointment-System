99027aeaa935f745eff8f5e22eb81cfa0969caeb
zts369 <zhupin_t_sio@dlsu.edu.ph>
2025-11-06 17:12:29 +0800

    Z-deleted

--- Clinic Appointment System/src/Util/Database.java
+++ Clinic Appointment System/src/Util/Database.java
@@ -7,16 +7,21 @@ public class Database {
 
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/medical_consultation_system";
     private static final String USER = "root";
-    private static final String PASSWORD = "sample";//ENTER YOUR MYSQL PASSWORD HERE
+    private static final String PASSWORD = "Patriotism#1";//ENTER YOUR MYSQL PASSWORD HERE
 
     // Establish connection with database
     public static Connection getConnection() {
         if (connection == null) {
             try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
                 connection = DriverManager.getConnection(URL, USER, PASSWORD);
                 System.out.println("Database connected!");
+            } catch (ClassNotFoundException e) {
+                System.out.println("MySQL JDBC Driver not found!");
+                e.printStackTrace();
             } catch (SQLException e) {
                 System.out.println("CANNOT CONNECT TO DATABASE!");
+                e.printStackTrace();
             }
         }
         return connection;
