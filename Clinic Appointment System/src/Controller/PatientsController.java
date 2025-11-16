c6171ca9be635e61d7428c65f56ade5d186bfc0e
harrisgarzonn <harris_martin_garzon@dlsu.edu.ph>
2025-11-16 23:55:15 +0800

    Update PatientsController.java

--- Clinic Appointment System/src/Controller/PatientsController.java
+++ Clinic Appointment System/src/Controller/PatientsController.java
@@ -287,4 +287,8 @@ public class PatientsController {
     public void openMedicineManagement(ActionEvent e) throws IOException {
         SceneManager.transition(e, "MedicineManagement");
     }
+
+    public void openMedicalHistory(ActionEvent e) throws IOException {
+        SceneManager.transition(e, "MedicalHistory");
+    }
 }
