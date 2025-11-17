f094cc2b50eccd2ae30ff1d4e2bbab415a0f0090
zts369 <zhupin_t_sio@dlsu.edu.ph>
2025-11-17 14:19:15 +0800

    Finished report for Specialization. Doctor and Admin scene for Prescription and Procedure.

--- Clinic Appointment System/src/Driver.java
+++ Clinic Appointment System/src/Driver.java
@@ -21,7 +21,7 @@ public class Driver extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/Scenes/Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/Scenes/Reports.fxml"));
         primaryStage.setTitle("Clinic Appointment System");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
